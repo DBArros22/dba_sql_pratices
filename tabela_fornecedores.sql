ff1eb422e1f5a595df702377bf95c9a033aaf4ae
DBArros22 <tinystoresofc@gmail.com>
2025-10-27 20:23:52 -0300

    Create tabela_fornecedores.sql
    tabela ficticia de dados referentes a fornecedores, com praticas de comandos para insercao de dados.

---
+++ tabela_fornecedores.sql
@@ -0,0 +1,19 @@
+CREATE TABLE fornecedores ( 
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  nome VARCHAR(100),
+  telefone VARCHAR(50),
+  cidade VARCHAR(100)
+  );
+ 
+INSERT INTO fornecedores(nome, telefone)
+VALUES ('EletronTech', '21581234',''Rio de Janeiro'),
+('Moveletron', '21581345','Sao Paulo),
+('GigaEletrônicos', '21581456','Minas Gerais'),
+('Casa do Eletrônico', '21581567', 'Paraiba'),
+('TechMóveis', '21581678', 'Pará'),
+('ElectroStore', '21581789', 'Coritiba'),
+('Móveis & Bits', '21581890', 'Espirito Santo'),
+('SmartHome Supplies', '21581901', 'Manaus'),
+('Eletrônica Brasil', '21582012', '),
+('Mundo Eletrônico', '21582123');
+  
